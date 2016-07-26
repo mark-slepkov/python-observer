0913d36dc3dbd7c5032d543e8bd977a27c494fce
mark <self@mark-slepkov.ru>
2016-07-26 12:23:29 +0300

    License was added

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 __author__ = 'mark'
 
 import os
+from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md')) as f:
     README = f.read()
-
 with open(os.path.join(here, 'LICENSE')) as f:
     LICENSE = f.read()
-
-
-from setuptools import setup, find_packages
 requires = []
 dependency_links = []
 setup(name='observer',
-      version='1.0.5',
+      version='1.0.6',
       description='A short library which is implements Observer pattern for any classes',
       long_description=README + '\n\n',
       classifiers=[
